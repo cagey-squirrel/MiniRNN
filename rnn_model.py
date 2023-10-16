24b17ac7ff1f7c9f9cafc38c299e9ba665edb449
cagey-squirrel <cvetkovicaleksandar1302@gmail.com>
2023-10-16 20:46:12 +0200

    Support for GPU training

--- rnn_model.py
+++ rnn_model.py
@@ -1,37 +1,46 @@
 import torch
 
 
+class RNN_Parallel(torch.nn.DataParallel):
+
+    def __init__(self, module,  dim=0, device_ids=None, output_device=None):
+        super().__init__(module, device_ids, output_device, dim)
+        self.module = module
+    
+    def reset_hidden_state(self):
+        self.module.reset_hidden_state()
+
 
 
 class RNN(torch.nn.Module):
 
-    def __init__(self, char_to_id, hidden_size=1, vocab_size=1):
+    def __init__(self, char_to_id, device, hidden_size=1, vocab_size=1):
         super().__init__()
         self.char_to_id = char_to_id
+        self.device = device
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
-
         # Weights
-        self.Wxh = torch.nn.Parameter(data=torch.randn(size=(hidden_size, vocab_size)) * 0.01, requires_grad=True)
-        self.Whh = torch.nn.Parameter(data=torch.randn(size=(hidden_size, hidden_size)) * 0.01, requires_grad=True)
-        self.Why = torch.nn.Parameter(data=torch.randn(size=(vocab_size, hidden_size)) * 0.01, requires_grad=True)
+        self.Wxh = torch.nn.Parameter(data=torch.randn(size=(hidden_size, vocab_size)).to(device) * 0.01, requires_grad=True)
+        self.Whh = torch.nn.Parameter(data=torch.randn(size=(hidden_size, hidden_size)).to(device) * 0.01, requires_grad=True)
+        self.Why = torch.nn.Parameter(data=torch.randn(size=(vocab_size, hidden_size)).to(device) * 0.01, requires_grad=True)
 
         # Biases
-        self.bh = torch.nn.Parameter(data=torch.zeros((hidden_size, 1)), requires_grad=True)
-        self.by = torch.nn.Parameter(data=torch.zeros((vocab_size, 1)), requires_grad=True)
+        self.bh = torch.nn.Parameter(data=torch.zeros((hidden_size, 1)).to(device), requires_grad=True)
+        self.by = torch.nn.Parameter(data=torch.zeros((vocab_size, 1)).to(device), requires_grad=True)
 
         # Hidden state
-        self.h = torch.zeros((hidden_size, 1))
+        self.h = torch.zeros((hidden_size, 1)).to(device)
 
 
     def reset_hidden_state(self):
-        self.h = torch.zeros((self.hidden_size, 1))
+        self.h = torch.zeros((self.hidden_size, 1)).to(self.device)
         
     
     def forward(self, input_char):
         # Input vector has all 0s except index of input char which has a value of 1
         # Basically, its a one-hot encoding
-        input_vector = torch.zeros((self.vocab_size, 1))
+        input_vector = torch.zeros((self.vocab_size, 1)).to(self.device)
         # input_char_id = self.char_to_id[input_char]
         input_char_id = input_char
         input_vector[input_char_id] = 1
--- train.py
+++ train.py
@@ -1,26 +1,29 @@
-from rnn_model import RNN
+from rnn_model import RNN, RNN_Parallel
 from loss import CharacterPredictionLoss
 import torch
-from util import load_data
+from util import load_data, sample_data
+from time import time
 
 
+def train(data_path, num_epochs=1000, lr=0.01, seq_len=25, hidden_size=100, output_file_name='output.txt'):
 
-
-
-def train(data_path, num_epochs=100, lr=0.1, seq_len=25, hidden_size=100):
-
+    device = torch.device('cpu' if not torch.cuda.is_available() else "cuda:0")
     data, data_size, vocab_size, char_to_id, id_to_char = load_data(data_path)
-    network = RNN(char_to_id, vocab_size=vocab_size, hidden_size=hidden_size)
+    network = RNN(char_to_id, device, vocab_size=vocab_size, hidden_size=hidden_size)
+    network.to(device)
+    network.train()
     loss_function = CharacterPredictionLoss(char_to_id)
     optimizer = torch.optim.Adam(network.parameters(), lr=lr)
 
-    network.train()
-
+    output_file = open(output_file_name, 'w')
+    
     for epoch in range(num_epochs):
         
+        time_start = time()
         loss = 0
         network.reset_hidden_state()
         p = 0
+
 
         while p+seq_len+1 <= data_size:
             
@@ -33,20 +36,27 @@ def train(data_path, num_epochs=100, lr=0.1, seq_len=25, hidden_size=100):
                 char_predictions = network(input_char)
                 loss += loss_function(char_predictions, target_char)
 
-
             p += seq_len
 
         loss /= seq_len
-        print(f'loss = {loss}')
+        print(f'finished epoch {epoch} with loss = {loss} in {time() - time_start} seconds')
         
         optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(network.parameters(), 5)
         optimizer.step()
+
+        if (epoch + 0) % 10 == 0:
+            sample_data(network, 'a', 100, output_file, char_to_id, id_to_char)
+            output_file.write('\n\n')
+            output_file.flush()
+
 
 
 
 def main():
-    path = 'aca_chat.txt'
+    path = 'aca_chat_mini.txt'
+    path = 'mini_example.txt'
     train(path)
 
 
