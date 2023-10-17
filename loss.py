1c80e0c430ada2fd3888995ec86923bbf2bafde8
cagey-squirrel <cvetkovicaleksandar1302@gmail.com>
2023-10-17 21:01:42 +0200

    Changing loss function to BCE

--- loss.py
+++ loss.py
@@ -3,10 +3,12 @@ import numpy as np
 
 class CharacterPredictionLoss(torch.nn.Module):
   
-    def __init__(self, char_to_id):
+    def __init__(self, char_to_id, device):
         super().__init__()
 
         self.char_to_id = char_to_id
+        self.device = device
+        self.loss_function = torch.nn.BCELoss()
 
     
     def forward(self, char_predictions, target_char):
@@ -15,54 +17,8 @@ class CharacterPredictionLoss(torch.nn.Module):
         We expect the index of target_char to have probability 1 
         '''
         
-        #target_index = self.char_to_id[target_char]
-        target_index = target_char
-        loss = -torch.log(char_predictions[target_index])
-        return loss
-
-
-
-
-
-
-
-
-def lossFun(inputs, targets, hprev):
-    Wxh, Whh, Why = 0, 0, 0
-    vocab_size = 1
-    bh = 0
-    by = 0
-    """
-    inputs,targets are both list of integers.
-    hprev is Hx1 array of initial hidden state
-    returns the loss, gradients on model parameters, and last hidden state
-    """
-    xs, hs, ys, ps = {}, {}, {}, {}
-    hs[-1] = np.copy(hprev)
-    loss = 0
-    # forward pass
-    for t in range(len(inputs)):
-        xs[t] = np.zeros((vocab_size,1)) # encode in 1-of-k representation
-        xs[t][inputs[t]] = 1
-        hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
-        ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next chars
-        ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
-        loss += -np.log(ps[t][targets[t],0]) # softmax (cross-entropy loss)
-  # backward pass: compute gradients going backwards
-    dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-    dbh, dby = np.zeros_like(bh), np.zeros_like(by)
-    dhnext = np.zeros_like(hs[0])
-    for t in reversed(range(len(inputs))):
-        dy = np.copy(ps[t])
-        dy[targets[t]] -= 1 # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
-        dWhy += np.dot(dy, hs[t].T)
-        dby += dy
-        dh = np.dot(Why.T, dy) + dhnext # backprop into h
-        dhraw = (1 - hs[t] * hs[t]) * dh # backprop through tanh nonlinearity
-        dbh += dhraw
-        dWxh += np.dot(dhraw, xs[t].T)
-        dWhh += np.dot(dhraw, hs[t-1].T)
-        dhnext = np.dot(Whh.T, dhraw)
-    for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-        np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
-    return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs)-1]
+        target_index = target_char.flatten()
+        targets = torch.zeros(char_predictions.shape, device=self.device)
+        targets[torch.arange(targets.shape[0]), target_index, 0] = 1
+        loss_mean = self.loss_function(char_predictions, targets)
+        return loss_mean
