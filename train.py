b6cc4363ab28aa736d1bcf870abb4e55723c81ef
cagey-squirrel <cvetkovicaleksandar1302@gmail.com>
2023-10-17 21:03:43 +0200

    Added Pytorch dataloaders and paralelized training

--- train.py
+++ train.py
@@ -1,53 +1,58 @@
 from rnn_model import RNN, RNN_Parallel
 from loss import CharacterPredictionLoss
 import torch
-from util import load_data, sample_data
+from util import load_data, sample_data, get_char_data_loaders
 from time import time
+import sys
+from IPython.core.ultratb import ColorTB
+
+sys.excepthook = ColorTB()
 
 
-def train(data_path, num_epochs=1000, lr=0.01, seq_len=25, hidden_size=100, output_file_name='output.txt'):
+def train(data_path, num_epochs=1000, lr=0.01, sequence_length=1024, hidden_size=100, output_file_name='output.txt'):
 
     device = torch.device('cpu' if not torch.cuda.is_available() else "cuda:0")
-    data, data_size, vocab_size, char_to_id, id_to_char = load_data(data_path)
+    char_index_data, data_size, vocab_size, char_to_id, id_to_char = load_data(data_path)
+    char_dataloader, num_sequences = get_char_data_loaders(char_index_data, data_size, sequence_length)
+    
     network = RNN(char_to_id, device, vocab_size=vocab_size, hidden_size=hidden_size)
     network.to(device)
     network.train()
-    loss_function = CharacterPredictionLoss(char_to_id)
+    loss_function = CharacterPredictionLoss(char_to_id, device)
     optimizer = torch.optim.Adam(network.parameters(), lr=lr)
 
-    output_file = open(output_file_name, 'w')
+    output_file = open(output_file_name, 'w', encoding='utf-8')
     
     for epoch in range(num_epochs):
         
-        time_start = time()
-        loss = 0
-        network.reset_hidden_state()
-        p = 0
-
-
-        while p+seq_len+1 <= data_size:
-            
-            inputs  = [char_to_id[char] for char in data[p:p+seq_len]]
-            targets = [char_to_id[char] for char in data[p+1:p+seq_len+1]]
-            
-            
-
-            for input_char, target_char in zip(inputs, targets):
-                char_predictions = network(input_char)
-                loss += loss_function(char_predictions, target_char)
-
-            p += seq_len
-
-        loss /= seq_len
-        print(f'finished epoch {epoch} with loss = {loss} in {time() - time_start} seconds')
-        
         optimizer.zero_grad()
-        loss.backward()
+        network.train()
+        time_start = time()
+        total_loss = 0
+        loads = 0
+
+        hidden_state = torch.zeros((num_sequences, hidden_size, 1)).to(device)
+
+        for inputs, targets in char_dataloader:
+            optimizer.zero_grad()
+            loads += 1
+
+            outputs, hidden_state = network(inputs, hidden_state)
+            hidden_state = hidden_state.detach()
+            
+            loss = loss_function(outputs, targets)
+            total_loss += loss
+            
+        total_loss /= loads
+       
+        print(f'finished epoch {epoch} with loss = {total_loss} in {time() - time_start} seconds')
+        
+        total_loss.backward()
         torch.nn.utils.clip_grad_norm_(network.parameters(), 5)
         optimizer.step()
 
         if (epoch + 0) % 10 == 0:
-            sample_data(network, 'a', 100, output_file, char_to_id, id_to_char)
+            sample_data(network, 'a', 100, output_file, char_to_id, id_to_char, hidden_size, device)
             output_file.write('\n\n')
             output_file.flush()
 
@@ -55,8 +60,9 @@ def train(data_path, num_epochs=1000, lr=0.01, seq_len=25, hidden_size=100, outp
 
 
 def main():
-    path = 'aca_chat_mini.txt'
-    path = 'mini_example.txt'
+    path = 'aca_chat.txt'
+    #path = 'mini_example.txt'
+    #path = 'test.txt'
     train(path)
 
 
--- util.py
+++ util.py
@@ -1,6 +1,36 @@
 import torch 
+from torch.utils.data import DataLoader
 
 
+class CharDataset(torch.utils.data.Dataset):
+
+    def __init__(self, char_index_data, sequence_length, data_size):
+        self.char_index_data = char_index_data
+        self.sequence_length = sequence_length
+        self.data_size = data_size
+        self.num_sequences = (len(self.char_index_data) - 1) // self.sequence_length
+        print(f'self.data_size = {self.data_size}')
+        print(f'self.num_sequences = {self.num_sequences}')
+        print(f'self.sequence_length = {self.sequence_length}')
+
+    def __len__(self):
+        return self.sequence_length
+    
+    
+    def __getitem__(self, index):
+        
+        inputs  = [char_index for char_index in self.char_index_data[index :  : self.sequence_length]][:self.num_sequences]
+        targets = [char_index for char_index in self.char_index_data[(index + 1) :  : self.sequence_length]][:self.num_sequences]
+
+        return torch.Tensor(inputs).long(), torch.Tensor(targets).long()
+
+
+def get_char_data_loaders(char_index_data, data_size, sequence_length):
+
+    char_dataset = CharDataset(char_index_data, sequence_length, data_size)
+    char_dataloader = DataLoader(char_dataset, batch_size=1, shuffle=False)
+
+    return char_dataloader, char_dataset.num_sequences
 
 
 def load_data(path):
@@ -13,29 +43,33 @@ def load_data(path):
     char_to_ix = { ch:i for i,ch in enumerate(unique_chars) }
     ix_to_char = { i:ch for i,ch in enumerate(unique_chars) }
 
-    return char_data, data_size, vocab_size, char_to_ix, ix_to_char
+    char_index_data = [char_to_ix[char] for char in char_data]
+
+    return char_index_data, data_size, vocab_size, char_to_ix, ix_to_char
 
 
-def sample_data(model, seed, length, output_file, char_to_id, id_to_char):
+def sample_data(network, seed, length, output_file, char_to_id, id_to_char, hidden_size, device):
     '''
     Samples a string of length 'length' from model and prints it to output file
     Seed 'seed' is used to prompt the first iteration of sampling 
     Char_to_id is mapping from char ('c') to index (5)
     id_to_char is mapping from char (5) to char ('c')
     '''
+    network.eval()
 
     input_char = seed
     input_char_index = char_to_id[input_char]
     previous_char_index = input_char_index
+    hidden_state = torch.zeros((1, hidden_size, 1)).to(device)
 
     for _ in range(length):
 
-        next_char_probabilities = model(previous_char_index)
+        next_char_probabilities, hidden_state = network(torch.Tensor([previous_char_index]).long(), hidden_state)
         next_char_index = torch.argmax(next_char_probabilities).item()
         next_char = id_to_char[next_char_index]
         output_file.write(next_char)
 
         previous_char_index = next_char_index
 
-    
+    network.train()
 
